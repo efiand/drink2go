75766b2a09878e54791131cebd9dcf0b0382da11
efiand <efiand@users.noreply.github.com>
2022-10-12 18:50:20 +0000

    deploy: 3dcc7fe70bb6b5951af985b0200e305007d3c2a6

--- js/script.js
+++
@@ -1,9 +0,0 @@
-const navElement = document.querySelector(".page-header__nav");
-const hamburgerElement = navElement.querySelector(".page-header__link--hamburger");
-
-hamburgerElement.addEventListener("click", () => {
-  const expanded = hamburgerElement.getAttribute("aria-expanded");
-  hamburgerElement.setAttribute("aria-expanded", expanded === "true" ? "false" : "true");
-});
-
-navElement.classList.remove("page-header__nav--initial");
